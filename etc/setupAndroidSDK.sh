70cfb97a8cccad96a7cc886d81c5d68e1b61390e
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-01-31 11:58:55 +0100

    Update android SDK

--- etc/setupAndroidSDK.sh
+++ etc/setupAndroidSDK.sh
@@ -11,4 +11,5 @@ echo "# Creating AVD"
 echo ""
 
 echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a
-emulator -avd test -no-skin -no-audio -no-window > /dev/null 2>&1 &
+#emulator -avd test -no-skin -no-audio -no-window &
+#wait_for_emulator
