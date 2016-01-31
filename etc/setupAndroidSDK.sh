fdea406357ec6921c7c87e165f6a375d091a9a14
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-01-31 12:00:13 +0100

    Update android SDK

--- etc/setupAndroidSDK.sh
+++ etc/setupAndroidSDK.sh
@@ -3,13 +3,13 @@ echo "# Executing https://raw.github.com/embarkmobile/android-sdk-installer"
 echo ""
 
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer \
-  | bash /dev/stdin --install=build-tools-22.0.1,android-22,sys-img-armeabi-v7a-android-22 \
+  | bash /dev/stdin --install=build-tools-23.0.2,android-23,sys-img-armeabi-v7a-android-23 \
   && source ~/.android-sdk-installer/env
 
 echo ""
 echo "# Creating AVD"
 echo ""
 
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
-#emulator -avd test -no-skin -no-audio -no-window &
+echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
 #wait_for_emulator
